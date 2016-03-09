b36e0f7deaeb21425a21b4caf02b33ec266157e8
Matt Paul <mattpaul001@icloud.com>
2016-03-09 10:43:47 +0000

    Robot can move forward one step in every orientation

--- spec/robotSpec.js
+++ spec/robotSpec.js
@@ -14,10 +14,40 @@ describe('Robot', function() {
 
     it('should have a starting orientation', function() {
       expect(rob.orientation).toBeDefined();
-    })
+    });
 
     it('should have some mars roving instructions', function() {
       expect(rob.instructions).toBeDefined();
-    })
+    });
+
+    it('should process instruction string into an array', function()  {
+    expect(rob.instructions).toEqual(['F','F']);
+    });
+
   });
+
+  describe('moving forward', function() {
+
+    it('should move north', function() {
+      rob.moveForward([2,2], 'N');
+      expect(rob.coordinates).toEqual([2,3]);
+    });
+
+    it('should move south', function() {
+      rob.moveForward([2,2], 'S');
+      expect(rob.coordinates).toEqual([2,1]);
+    });
+
+    it('should move east', function() {
+      rob.moveForward([2,2], 'E');
+      expect(rob.coordinates).toEqual([3,2]);
+    });
+
+    it('should move west', function() {
+      rob.moveForward([2,2], 'W');
+      expect(rob.coordinates).toEqual([1,2]);
+    });
+
+  });
+
 });
--- src/robot.js
+++ src/robot.js
@@ -1,5 +1,24 @@
 function Robot (x,y,o,i) {
   this.coordinates = [x,y];
   this.orientation = o;
-  this.instructions = i;
+  this.instructions = i.split('');
 }
+
+Robot.prototype = {
+  constructor: Robot,
+
+  moveForward: function(coordinates, orientation) {
+    //add helper method to validate the orientation input
+    if(orientation === 'N') {
+      coordinates[1] +=1;
+    } else if (orientation === 'S') {
+      coordinates[1] -=1;
+    } else if (orientation === 'E') {
+      coordinates[0] +=1;
+    } else {
+      coordinates[0] -=1;
+    }
+    this.coordinates = coordinates;
+    }
+};
+
